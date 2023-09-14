528014fde5ac65583575a3976d56d48c13f21fcb
UJWAL-YESANSURE <144993696+UJWAL-YESANSURE@users.noreply.github.com>
2023-09-14 12:49:11 +0530

    Add files via upload
    Java Codes

---
+++ p34.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class p34
+{
+    public static void main(String[] args) 
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter a number");
+int n = sc.nextInt();
+int tem num;
+fact = 1;
+for(;num>=1;num--)
+{
+fact = fact*num;
+}
+println(the factorial of number is "+temp"is "+fact")
+}
+}
